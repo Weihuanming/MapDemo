0a3dfa1d603b6a4f275bdd07947ee7eceb82d9df
weihuanming <253844203@qq.com>
2016-12-29 17:28:07 +0800

    实现2d地图定位功能

--- app/src/main/java/test/mapdemo/MainActivity.java
+++ app/src/main/java/test/mapdemo/MainActivity.java
@@ -1,13 +1,124 @@
 package test.mapdemo;
 
+import android.graphics.BitmapFactory;
+import android.media.Rating;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.BitmapDescriptor;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private AMap aMap;
+
+    public AMapLocationClient mLocationClient;
+    public AMapLocationClientOption mLocationClientOption;
+    private double lat;
+    private double lon;
+
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
+            if (aMapLocation != null) {
+                if (aMapLocation.getErrorCode() == 0) {
+                    lat = aMapLocation.getLatitude();
+                    lon = aMapLocation.getLongitude();
+
+                    Log.d("pcw","lat: " + lat +", lon: " + lon);
+                    aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 19));
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(new LatLng(lat, lon));
+                    markerOptions.title("当前位置");
+                    markerOptions.visible(true);
+                    BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight));
+                    markerOptions.icon(bitmapDescriptor);
+                    aMap.addMarker(markerOptions);
+                }
+            } else {
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+
+        mLocationClient.setLocationListener(mLocationListener);
+
+        init();
+    }
+
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+        setUpMap();
+    }
+
+    private void setUpMap() {
+        mLocationClientOption = new AMapLocationClientOption();
+        mLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationClientOption.setNeedAddress(true);
+        mLocationClientOption.setOnceLocation(true);
+        mLocationClientOption.setWifiScan(true);
+        mLocationClientOption.setMockEnable(false);
+        mLocationClientOption.setInterval(2000);
+        mLocationClient.setLocationOption(mLocationClientOption);
+        mLocationClient.startLocation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mLocationClient.stopLocation();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        mLocationClient.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+        mLocationClient.onDestroy();
     }
 }
